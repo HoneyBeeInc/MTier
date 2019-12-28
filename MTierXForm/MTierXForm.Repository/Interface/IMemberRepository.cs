f039d7a2dfb545cfc8123eb6b39f8468fac565c6
evenegas <evenegas@mybenefitexpress.com>
2019-12-28 10:39:23 -0800

    #2

---
+++ MTierXForm/MTierXForm.Repository/Interface/IMemberRepository.cs
@@ -0,0 +1,12 @@
+﻿using MTier.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTierXForm.Repository.Interface
+{
+    public interface IMemberRepository
+    {
+        Member GetMember(int memberId);
+    }
+}
---
+++ MTierXForm/MTierXForm.Repository/MemberRepository.cs
@@ -0,0 +1,20 @@
+﻿using MTier.Domain;
+using MTierXForm.Repository.Interface;
+using System;
+
+namespace MTierXForm.Repository
+{
+    public class MemberRepository : IMemberRepository
+    {
+        public Member GetMember(int memberId)
+        {
+            return new Member()
+            {
+                MemberId = memberId,
+                FirstName = "Miguel",
+                LastName = "Jordan",
+                DateOfBirth = new DateTime(1980, 11, 20)
+            };
+        }
+    }
+}
